16a18de123a25b055e6d052e9df67c4847645d89
Tahmeed Ullah <tahmeedullah651@gmail.com>
2025-01-12 13:24:15 +0500

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL:
+    "https://clipverse-backend-eaasg7f0ambkc4cx.northeurope-01.azurewebsites.net/",
+});
+
+// Add request interceptor for auth token
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default API;
